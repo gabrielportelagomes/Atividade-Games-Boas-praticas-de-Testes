206d5b0369f209993213cb94d0c95b3719f1fed6
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-02-09 15:20:21 -0300

    feat: get game by id route tests

--- tests/integration/console.test.ts
+++ tests/integration/console.test.ts
@@ -10,8 +10,24 @@ const server = supertest(app);
 
 describe("GET /consoles", () => {
   it("should respond with status 200 with all consoles", async () => {
-    await createConsole();
+    const consoles =await createConsole();
     const response = await server.get("/consoles");
     expect(response.status).toBe(200);
+    expect(response.body).toEqual([{id: consoles.id,name:consoles.name}]);
   });
 });
+
+describe("GET /consoles/:id",()=>{
+    it("should respond with satus 200 if console with id exist",async ()=>{
+        const console = await createConsole();
+        const response = await server.get(`/consoles/${console.id}`);
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({id: console.id,name:console.name});
+    });
+    it("should respond with status 404 when the id given no exist",async()=>{
+        const response = await server.get("/consoles/0");
+        expect(response.status).toBe(404);
+    });
+
+})
+
